5e2235edbbeb61f18cf710d6b3fa444efae96314
Nicolas Lumineau <nicolas.lumineau@univ-lyon1.fr>
2025-09-15 14:20:30 +0200

    configuration

---
+++ init-db.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+TP_DB="cinema"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    create database "$TP_DB";
+    create user etu with password 'pwdetu';
+    GRANT ALL PRIVILEGES ON DATABASE "$TP_DB" to etu;
+EOSQL
+
+
+psql -v ON_ERROR_STOP=0 -d "$TP_DB" -f /bd-data.sql
